e141fbb3f392356b4172f6f02138156bb7474c33
PS-PRANAV4 <pranavpranab@gmail.com>
2023-12-13 14:37:03 +0530

    env varible added

--- Brain_Health/settings.py
+++ Brain_Health/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l49l+fewn!d0+(z&_@l$&#l112$gi=7sp_)!b4r++k@o^h^y03'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
